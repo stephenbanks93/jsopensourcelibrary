a9ebd2337105709ae727d9ee9242481d32e78a21
stephen banks <stephenbanks93@yahoo.com>
2016-07-14 12:02:26 -0400

    updated integers

---
+++ js/1.00_integers.js
@@ -0,0 +1,69 @@
+//VARIABLES//
+
+//As my first example, lets initialize a variable
+
+var z; //variable "z" is initialized
+       //but, it's value is "undefined".
+
+
+var y = 1; //This variable now has a value of one
+console.log("Variable declaration");
+console.log(y);
+
+
+//OPERATORS//
+
+//ADDITION//
+
+var addNum1 = 2; //addNum1 has the value of two
+var addNum2 = 5; //addNum2 has the value of five
+var addNum3 = addNum1 + addNum2; //addNum3 has the value of addNum1 plus addNum2
+console.log("Addition");                                 
+console.log(addNum3);
+
+//SUBTRACTION//
+
+var subNum1 = 10; //subNum1 has the value of ten
+var subNum2 = 6; //subNum2 has the value of six;
+var subNum3 = subNum1 - subNum2; //subNum3 has the value of subNum1 minus subNum2
+console.log("Subtraction");
+console.log(subNum3);
+
+//MULTIPLICATION//
+
+var multNum1 = 5; //multNum1 has the value of five
+var multNum2 = 6; //multNum2 has the value of six
+var multNum3 = multNum1 * multNum2; //multNum3 has the value of multNum1 times multNum2
+console.log("Multiplication");
+console.log(multNum3);
+
+//DIVISION//
+
+var divNum1 = 6; //divNum1 has the value of six
+var divNum2 = 3; //divNum2 has the value of three
+var divNum3 = divNum1 / divNum2; //divNum3 has the value of divNum1 divided by divNum2
+console.log("Division");
+console.log(divNum3);
+
+//DIVISION WITH MODULUS//
+//Division with modulus will only display the "Remainder"
+
+var modNum1 = 7;
+var modNum2 = 3;
+var modNum3 = modNum1 % modNum2;
+console.log("Modulus division");
+console.log(modNum3);
+
+//INCREMENTS//
+//Adding one
+
+var i = 2;
+var f = ++i;
+console.log("Increments +");
+console.log(f);
+
+//Subtracting one
+
+var t = --i;
+console.log("Increments -");
+console.log(t);
