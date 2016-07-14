7a37504223eb845617f10a8c91580e3dd2e42de0
stephen banks <stephenbanks93@yahoo.com>
2016-07-14 13:48:26 -0400

    Integers Done

--- js/1.00_integers.js
+++ js/1.00_integers.js
@@ -6,7 +6,7 @@ var z; //variable "z" is initialized
        //but, it's value is "undefined".
 
 
-var y = 1; //This variable now has a value of one
+var y = 1; //This variable now has a value of "one"
 console.log("Variable declaration");
 console.log(y);
 
@@ -15,33 +15,33 @@ console.log(y);
 
 //ADDITION//
 
-var addNum1 = 2; //addNum1 has the value of two
-var addNum2 = 5; //addNum2 has the value of five
-var addNum3 = addNum1 + addNum2; //addNum3 has the value of addNum1 plus addNum2
+var addNum1 = 2; //addNum1 has the value of "two"
+var addNum2 = 5; //addNum2 has the value of "five"
+var addNum3 = addNum1 + addNum2; //addNum3 has the value of "addNum1" plus "addNum2"
 console.log("Addition");                                 
 console.log(addNum3);
 
 //SUBTRACTION//
 
-var subNum1 = 10; //subNum1 has the value of ten
-var subNum2 = 6; //subNum2 has the value of six;
-var subNum3 = subNum1 - subNum2; //subNum3 has the value of subNum1 minus subNum2
+var subNum1 = 10; //"subNum1" has the value of "ten"
+var subNum2 = 6; //"subNum2" has the value of "six"
+var subNum3 = subNum1 - subNum2; //"subNum3" has the value of "subNum1" minus "subNum2"
 console.log("Subtraction");
 console.log(subNum3);
 
 //MULTIPLICATION//
 
-var multNum1 = 5; //multNum1 has the value of five
-var multNum2 = 6; //multNum2 has the value of six
-var multNum3 = multNum1 * multNum2; //multNum3 has the value of multNum1 times multNum2
+var multNum1 = 5; //"multNum1" has the value of "five"
+var multNum2 = 6; //"multNum2" has the value of "six"
+var multNum3 = multNum1 * multNum2; //"multNum3" has the value of "multNum1" times "multNum2"
 console.log("Multiplication");
 console.log(multNum3);
 
 //DIVISION//
 
-var divNum1 = 6; //divNum1 has the value of six
-var divNum2 = 3; //divNum2 has the value of three
-var divNum3 = divNum1 / divNum2; //divNum3 has the value of divNum1 divided by divNum2
+var divNum1 = 6; //"divNum1" has the value of "six
+var divNum2 = 3; //"divNum2" has the value of "three"
+var divNum3 = divNum1 / divNum2; //"divNum3" has the value of "divNum1" divided by "divNum2"
 console.log("Division");
 console.log(divNum3);
 
@@ -57,7 +57,7 @@ console.log(modNum3);
 //INCREMENTS//
 //Adding one
 
-var i = 2;
+var i = 2; //
 var f = ++i;
 console.log("Increments +");
 console.log(f);
@@ -67,3 +67,19 @@ console.log(f);
 var t = --i;
 console.log("Increments -");
 console.log(t);
+
+//Assignment Operators ("=" is an assignment operator)
+
+var k = 4;
+var j = 2;
+
+console.log("Assignment Operators");
+console.log(k += j); //Add
+console.log(k -= j); //Subtract
+console.log(k *= j); //Multiply
+console.log(k /= j); //Division
+
+var modK = 5;
+var modJ = 2;
+
+console.log(modK %= modJ); //Division With "Remainder Only"
