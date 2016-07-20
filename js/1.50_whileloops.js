55851b19240e50229a10a54d49048082734cc900
stephen banks <stephenbanks93@yahoo.com>
2016-07-20 14:10:25 -0400

    small commit

---
+++ js/1.50_whileloops.js
@@ -0,0 +1,20 @@
+//While Loops//
+
+console.log("While loop");
+var i = 1;
+
+while (i <= 5){		//While "i" is less than or equal to "5",
+	console.log(i); //console log the current value of "i" 
+	i++;            //Adds 1 to the current value of "i"
+};                    
+//Keeps looping until the value of "i" is equal to "5" then stops
+
+//Do While Loops//
+
+console.log("Do While loop")
+var x = 1;
+
+do {
+	console.log(x);
+	x++;
+} while (x <= 5);
