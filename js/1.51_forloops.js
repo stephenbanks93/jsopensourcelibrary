cc28e9b8685fc2cf5929b0f1b735b1549b91bca9
stephen banks <stephenbanks93@yahoo.com>
2016-07-20 15:56:10 -0400

    willcomebackto

---
+++ js/1.51_forloops.js
@@ -0,0 +1,33 @@
+//For Loops//
+console.log("example 1");
+
+for (x = 1; x <= 5; x++){
+
+	console.log(x);
+
+};
+
+
+//The first statment in a for loop is optional
+console.log("example 2");
+
+var y = 1;
+
+for (; y <= 5; y++){
+	console.log(y);
+};
+
+//For/In Loop//
+console.log("example 3");
+
+var car = {carName:"BMW", model:"M1", horsePower:"277hp"};
+
+for (i in car){
+	console.log(car[i]);
+};
+
+
+
+
+
+
